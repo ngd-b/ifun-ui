9af63c840b0e3e96af46012d702c727ce8f93a26
hboot <bobolity@163.com>
2022-09-09 11:32:37 +0800

    style: publish version

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "ci",
+        "style",
+        "docs",
+        "chore",
+        "revert",
+        "refactor",
+        "test",
+        "build",
+        "perf",
+      ],
+    ],
+  },
 };
